304cd1e1c04c19cbf4733a3d934ba55b4170a845
Anuj mishra <captainanuj2004@gmail.com>
2024-07-23 15:11:49 +0530

    maked About section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,8 +8,22 @@ function About(){
         <img src={theme} alt="theme"/>
         <div className="my-about">
             <img class="my-about-fullimage" src={image} alt="my-full-photo" />
+            <div className="aboutpara">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit optio odit unde tempora voluptas, commodi aut consectetur aspernatur quibusdam vitae est doloribus, accusantium reprehenderit ipsum ut eligendi debitis sunt nesciunt?</p>
-        </div>
-    </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae harum assumenda autem eveniet mollitia, odit sint nihil et vero modi exercitationem quibusdam, aliquam dolore.</p>
+            <div className="about-skills">
+                <div className="about-skill">
+                    <p>HTML & CSS</p><hr style={{width:"50%"}}/>
+                    </div>
+                <div className="about-skill">
+                    <p>Javascript</p><hr  style={{width:"60%"}}/>
+                </div>
+                <div className="about-skill">
+                    <p>React JS</p><hr  style={{width:"70%"}}/>
+                </div>
+            </div>
+            </div>
+            </div>
+            </div>
 }
 export default About;
